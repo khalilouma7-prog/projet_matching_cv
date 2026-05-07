f440bd991b007afef3bba7af4b08eab53c317c0a
Mohssinechalati <Mohssine.chalati.07@gmail.com>
2026-05-07 16:35:07 +0100

    Remove unused apps

--- backend/matching/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import ResultatMatching
-
-@admin.register(ResultatMatching)
-class ResultatMatchingAdmin(admin.ModelAdmin):
-    list_display = ['utilisateur', 'offre', 'score_total', 'date_calcul']
